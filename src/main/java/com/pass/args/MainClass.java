a5a2c3b1d478fb8a7ce09dbfebc08c545010529a
Mohammad Zaman <mohammad.zaman@carrier.com>
2021-03-17 21:58:20 -0400

    initial commit

---
+++ src/main/java/com/pass/args/MainClass.java
@@ -0,0 +1,7 @@
+package com.pass.args;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Executing from the main class, args.length: " + args.length);
+    }
+}
