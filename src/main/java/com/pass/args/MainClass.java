5ffa7d57722ec4f96b1ab574f85c5a270d383625
Mohammad Zaman <mohammad.zaman@carrier.com>
2021-03-17 22:55:54 -0400

    testing passing project properties to main method form the gradle task

--- src/main/java/com/pass/args/MainClass.java
+++ src/main/java/com/pass/args/MainClass.java
@@ -1,7 +1,22 @@
 package com.pass.args;
 
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Executing from the main class, args.length: " + args.length);
+//        System.out.println("Executing from the main class, args.length: " + args.length);
+
+        if (args.length == 0) {
+            System.exit(0);
+        }
+
+        if (args.length == 1) {
+            System.out.println(args[0]);
+            String[] s = args[0].split(" ");
+            Arrays.stream(s).forEach(System.out::println);
+            return;
+        }
+
+        Arrays.stream(args).forEach(System.out::println);
     }
 }
